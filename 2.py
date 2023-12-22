355ca7fc284fc5b10577e916004a2506b8c900e8
leshchesh <153865461+leshchesh@users.noreply.github.com>
2023-12-22 05:16:18 +0300

    Add files via upload

--- 2.py
+++ 2.py
@@ -1,27 +1,11 @@
-import random
-from random import randint, uniform
-try:
-    n, m = int(input()), int(input())
-    if n > 0 and m > 0:
-        matrix = [[random.randint(1, 10) for _ in range(m)] for _ in range(n)] #Менять значение
-        m1, m2 = [[0 for j in range(len(matrix))] for i in range(len(matrix[0]))], [[0 for i in range(m)] for j in range(n)]
-        print('Исходная матрица')
-        for row in matrix:
-            print(*row, sep='  ')
-        print()
-        if len(matrix) == 1:
-            print('Измененная матрица')
-            for row in matrix:
-                print(*row, sep='  ')
-        else:
-            for i in range(1, len(matrix), 2):
-                mat = list()
-                for j in range(len(matrix[i]) - 1, -1, -1):
-                    mat.append(matrix[i][j])
-                matrix[i] = mat
-            print()
-            print('Измененная матрица')
-            for row in matrix:
-                print(*row, sep='  ')
-except ValueError:
-    print()
+def addleftdigit(d, к):
+    к = к * 10 + d
+    return к
+число = int(input("Введите число: "))
+while True:
+    цифра = int(input("Введите цифру для добавления: "))
+    число = addleftdigit(цифра, число)
+    print("Результат:", число)
+    продолжить = input("Хотите добавить еще цифру? (да/нет): ")  
+    if продолжить.lower() != "да":
+        break
